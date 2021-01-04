b2d47ff1ed067892194af51dcdca472f88fb6876
SiobhanBaines <siobhan.baines@gmail.com>
2021-01-04 16:16:03 +0000

    failed

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-let testA = document.getElementsByClassName('box')[0];
-let testB = document.getElementsByClassName('box')[1];
-let testC = document.getElementsByClassName('box')[2];
-let testD = document.getElementsByClassName('box')[3];
-let testE = document.getElementsByClassName('box')[4];
-let testF = document.getElementsByClassName('box')[5];
+let boxArray = document.getElementsByClassName('box');
 
 function boxClicked(){
-    let boxArray = document.getElementsByClassName('box');
-    //console.log(event);
-    console.log(boxArray);
-    console.log(testA.innerHTML);
+    for (let i =0; i < boxArray.length; i++){
+        console.log(boxArray[i].style.backgroundColor);
+    }
 
-    let colour = box.style.backgroundColor;
-    console.log(colour);
     if (colour === 'green')
     {
         box.style.backgroundColor = 'orange';
     }
 }
-testA.addEventListener('click', boxClicked);
-testB.addEventListener('click', boxClicked);
-testC.addEventListener('click', boxClicked);
-testD.addEventListener('click', boxClicked);
-testE.addEventListener('click', boxClicked);
-testF.addEventListener('click', boxClicked);
+boxArray.addEventListener('click', boxClicked);
